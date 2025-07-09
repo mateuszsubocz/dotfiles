a3cbc34e02ae77fdafa81841de6a0d75e9fb2cd4
mateuszsubocz <mateusz.subocz@computer4u.com>
2025-07-09 15:59:11 +0200

    Update installarch.sh

--- installarch.sh
+++ installarch.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
 #install needed packages
-sudo pacman -S --needed wofi firefox waypaper swaybg gammastep python-pywal pavucontrol waybar foot
+sudo pacman -S --needed wofi firefox swaybg gammastep python-pywal pavucontrol waybar foot
+yay waypaper
+
+#copy files to .config
+cd dotfiles
+cp river ~/.config
+cp wofi ~/.config
+cp waybar ~/.config
+cp waypaper ~/.config
+cp installarch.sh ~/.config
+cd .config
 
 #The installing script making go brrrr
 if [ -f installarch.sh ]; then
   chmod +x installarch.sh
   ./installarch.sh
 fi
-
-#copy files to .config
-cp -rf * .[!.]* ~/.config/
 
 #remove the dotfiles
 rm -rf dotfiles
